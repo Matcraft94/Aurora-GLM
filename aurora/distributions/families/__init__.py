6506d763957310fb57aeffbe5ed55ae859815bd9
Lucy Arias <101682688+Matcraft94@users.noreply.github.com>
2025-11-02 00:27:58 -0500

    Add gamma family and inverse link coverage

--- aurora/distributions/families/__init__.py
+++ aurora/distributions/families/__init__.py
@@ -1,7 +1,13 @@
 """Built-in distribution families."""
 
 from .binomial import BinomialFamily
+from .gamma import GammaFamily
 from .gaussian import GaussianFamily
 from .poisson import PoissonFamily
 
-__all__ = ["GaussianFamily", "BinomialFamily", "PoissonFamily"]
+__all__ = [
+	"GaussianFamily",
+	"BinomialFamily",
+	"PoissonFamily",
+	"GammaFamily",
+]
