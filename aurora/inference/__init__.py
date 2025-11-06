f97f374d0a1a66e11ee6b43519498e28380f00a6
Lucy <mat.eduardo.arias@outlook.com>
2025-11-05 21:08:21 -0500

    feat(robust): add robust inference methods including robust covariance and bootstrap inference

--- aurora/inference/__init__.py
+++ aurora/inference/__init__.py
@@ -3,11 +3,21 @@
 from .diagnostics import GLMDiagnosticResult, glm_diagnostics
 from .hypothesis import wald_test
 from .intervals import ConfidenceIntervalResult, confidence_intervals
+from .robust import (
+    HCType,
+    RobustInferenceResult,
+    bootstrap_inference,
+    robust_covariance,
+)
 
 __all__ = [
-	"confidence_intervals",
-	"ConfidenceIntervalResult",
-	"glm_diagnostics",
-	"GLMDiagnosticResult",
-	"wald_test",
+    "confidence_intervals",
+    "ConfidenceIntervalResult",
+    "glm_diagnostics",
+    "GLMDiagnosticResult",
+    "wald_test",
+    "robust_covariance",
+    "bootstrap_inference",
+    "RobustInferenceResult",
+    "HCType",
 ]
